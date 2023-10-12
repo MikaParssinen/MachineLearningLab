c826a32e62dd247ed515bd3352b2c8e8c263a3ea
Mika Pärssinen <113350989+MikaParssinen@users.noreply.github.com>
2023-10-12 15:53:20 +0200

    Revert "Update LinearRegression.h"
    This reverts commit bf24364bc0927c2b69c12a6ca6f637bd517ee07b.

--- MachineLearningLab/src/Regression/LinearRegression.h
+++ MachineLearningLab/src/Regression/LinearRegression.h
@@ -36,33 +36,6 @@ private:
     Eigen::VectorXd theta;
     Eigen::VectorXd m_coefficients; // Store the coefficients for future predictions
 
-    Eigen::VectorXd m_mean;  // Mean of each feature
-    Eigen::VectorXd m_std;   // Standard deviation of each feature
-    Eigen::VectorXd m_coefficients;  // Coefficients of the linear regression model
-
-    // Helper function to calculate mean and standard deviation of each feature
-    void calculateMeanAndStd(const std::vector<std::vector<double>>& trainData) {
-        int num_feats = trainData[0].size();
-        int num_samples = trainData.size();
-
-        m_mean = Eigen::VectorXd(num_feats);
-        m_std = Eigen::VectorXd(num_feats);
-
-        for (int i = 0; i < num_feats; ++i) {
-            double sum = 0.0;
-            for (int j = 0; j < num_samples; ++j) {
-                sum += trainData[j][i];
-            }
-            m_mean(i) = sum / num_samples;
-
-            double sum_squared_diff = 0.0;
-            for (int j = 0; j < num_samples; ++j) {
-                sum_squared_diff += (trainData[j][i] - m_mean(i)) * (trainData[j][i] - m_mean(i));
-            }
-            m_std(i) = std::sqrt(sum_squared_diff / num_samples);
-        }
-    }
-
 };
 
 #endif // LINEARREGRESSION_H
